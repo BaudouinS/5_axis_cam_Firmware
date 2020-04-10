b775a5263b14c4edf5cec5409e2d5a1ba1ab7500
BaudouinS <46686117+BaudouinS@users.noreply.github.com>
2020-04-10 13:11:45 -0500

    Create Display_SerialComputer.h

---
+++ 5axisCAM_ArdFirmware_v1-3_TMC2130_PS2CTRL/Display_SerialComputer.h
@@ -0,0 +1,23 @@
+/*
+  Display_SerialComputer - 
+  Created by B Saintyves, April 4, 2020.
+*/
+
+
+#ifndef Display_SerialComputer_h
+#define Display_SerialComputer_h
+
+#define Display_Timer 2000 //time in ms between 2 displays
+
+class fclass2
+{
+  public:
+    fclass2();
+    void Cartesian();
+    void Spherical();
+ 
+};
+  
+  extern fclass2 Display_SerialComputer;
+
+#endif
